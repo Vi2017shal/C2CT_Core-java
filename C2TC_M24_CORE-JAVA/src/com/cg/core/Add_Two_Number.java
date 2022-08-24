e9c3897fed971302a05a4e4cf6a97c0a071735d3
VISHAL <VISHAL@LAPTOP-71BMR2KQ>
2022-08-24 13:11:03 +0530

    first commit

---
+++ C2TC_M24_CORE-JAVA/src/com/cg/core/Add_Two_Number.java
@@ -0,0 +1,18 @@
+// WAP To sum of Two Number
+
+ package com.cg.core;
+ import java.util.Scanner;
+ public class Add_Two_Number {
+
+    public static void main(String[] args) {
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter 1st Number");
+	 int a=sc.nextInt();
+	 System.out.println("Enter 2nd Number");
+	 int b=sc.nextInt();
+	 int sum = a+b;
+	 System.out.println("Sum of two numbers:"+sum);
+	
+   }
+
+}
